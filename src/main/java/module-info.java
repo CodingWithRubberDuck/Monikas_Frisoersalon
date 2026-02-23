70b51f9c8cf0cfbae0d977937bba8ba1ef0e6078
josef <josefinebechfrandsen@gmail.com>
2026-02-23 13:38:40 +0100

    Opsætning af klasserne

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,10 @@ module org.example.monikas_frisoersalon {
 
     opens org.example.monikas_frisoersalon to javafx.fxml;
     exports org.example.monikas_frisoersalon;
+    exports dal;
+    opens dal to javafx.fxml;
+    exports logic;
+    opens logic to javafx.fxml;
+    exports ui;
+    opens ui to javafx.fxml;
 }
