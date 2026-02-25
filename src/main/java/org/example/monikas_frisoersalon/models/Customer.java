74124754116ca51560f8c25eefdc97d91b6f1ccc
josef <josefinebechfrandsen@gmail.com>
2026-02-25 13:34:55 +0100

    Rettet Hairdresser og Customer klasserne, så de stemmer overens med Person

--- src/main/java/org/example/monikas_frisoersalon/models/Customer.java
+++ src/main/java/org/example/monikas_frisoersalon/models/Customer.java
@@ -2,7 +2,7 @@ package org.example.monikas_frisoersalon.models;
 
 public class Customer extends Person{
 
-    public Customer(String name, int phoneNumber, String email, String password) {
-        super(name, phoneNumber, email, password);
+    public Customer(int id, String name, String phoneNumber, String email, String password) {
+        super(id, name, phoneNumber, email, password);
     }
 }
--- src/main/java/org/example/monikas_frisoersalon/models/Hairdresser.java
+++ src/main/java/org/example/monikas_frisoersalon/models/Hairdresser.java
@@ -2,7 +2,7 @@ package org.example.monikas_frisoersalon.models;
 
 public class Hairdresser extends Person{
 
-    public Hairdresser(String name, int phoneNumber, String email, String password) {
-        super(name, phoneNumber, email, password);
+    public Hairdresser(int id, String name, String phoneNumber, String email, String password) {
+        super(id, name, phoneNumber, email, password);
     }
 }
