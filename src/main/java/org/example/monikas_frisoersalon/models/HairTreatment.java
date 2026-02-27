767a3e8e3bac156d6ea2d921ea604e21a74d7ec2
augus <august.soeltoft@gmail.com>
2026-02-27 14:09:00 +0100

    Scenebuilder Booking-view er blevet sat op

--- src/main/java/org/example/monikas_frisoersalon/models/HairTreatment.java
+++ src/main/java/org/example/monikas_frisoersalon/models/HairTreatment.java
@@ -1,4 +1,13 @@
 package org.example.monikas_frisoersalon.models;
 
 public class HairTreatment {
+    private int id;
+    private String name;
+    private int duration;
+
+    public HairTreatment(int id, String name, int duration){
+        this.id = id;
+        this.name = name;
+        this.duration = duration;
+    }
 }
