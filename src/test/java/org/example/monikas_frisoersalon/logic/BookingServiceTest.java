59b6b7e5305bfc1734ac9aaa2adf5f4cba342e46
josef <josefinebechfrandsen@gmail.com>
2026-03-05 12:09:07 +0100

    Tilføjet test klassen BookingServiceTest

---
+++ src/test/java/org/example/monikas_frisoersalon/logic/BookingServiceTest.java
@@ -0,0 +1,52 @@
+package org.example.monikas_frisoersalon.logic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookingServiceTest {
+
+    @Test
+    void handleShowAllHairdressers() {
+    }
+
+    @Test
+    void handleGetAllHairTreatments() {
+    }
+
+    @Test
+    void handleGetAllBookings() {
+    }
+
+    @Test
+    void handleGetBookingsByDate() {
+    }
+
+    @Test
+    void handleUpdateBooking() {
+    }
+
+    @Test
+    void validateAddTreatment() {
+    }
+
+    @Test
+    void validateBooking() {
+    }
+
+    @Test
+    void handlePersonExists() {
+    }
+
+    @Test
+    void handleAddPerson() {
+    }
+
+    @Test
+    void handleAddCustomer() {
+    }
+
+    @Test
+    void handleAddBooking() {
+    }
+}
